4689e38b7271589acdcc1d1bb8e0e6b941728dc3
abdulraheem5335 <abdurraheem5335@gmail.com>
2025-12-06 01:41:21 +0500

    Fix Vercel deployment - simplify API handler and vercel.json

--- api/index.js
+++ api/index.js
@@ -1,13 +1,4 @@
-const app = require('../../src/server');
-
 // Vercel serverless function handler
-module.exports = (req, res) => {
-  // Handle OPTIONS request for CORS preflight
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
+const app = require('../src/server');
 
-  // Pass request to Express app
-  app(req, res);
-};
+module.exports = app;
